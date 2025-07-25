c6abb462e1cac0bb644e02f61770893d4a54d1b0
sebasrodusa <100875955+sebasrodusa@users.noreply.github.com>
2025-07-25 12:58:41 -0400

    Use BrowserRouter for routing and add Netlify redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 import './index.css';
@@ -9,9 +9,9 @@ import './App.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
